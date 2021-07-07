560dbae5211cf1ceccfc872ef9f4b926af35f2b8
tikgithub <tikgithub@gmail.com>
2021-07-07 16:35:15 +0700

    First Commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="card" style=" position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+color: white;">
+    <div class="card-footer text-muted text-right">
+      ©2021 ChipWork.COM
+    </div>
+  </div>
